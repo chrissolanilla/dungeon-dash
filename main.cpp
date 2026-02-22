d7ca0855db06a631605afbb0b8c16bebcb4a8649
Christopher Solanilla <chrissolanilla@gmail.com>
2026-02-22 02:31:47 -0500

    adds 2 player mode

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <vector>
 #include <unistd.h>
+#include <memory>
+#include <termios.h>
+
+static void ansi_clear_and_home() {
+    std::cout << "\x1b[2J\x1b[H";
+}
 
 static int readKey() {
 	unsigned char c;
@@ -10,6 +16,76 @@ static int readKey() {
 		return c;
 	return -1;
 }
+enum class key_code {
+    none,
+    esc,
+    up,
+    down,
+    left,
+    right,
+    w, a, s, d
+};
+
+static key_code read_key_code() {
+    int c = readKey();
+    if (c == -1) return key_code::none;
+
+    if (c == 27) {
+        int c1 = readKey();
+        if (c1 == -1) return key_code::esc;
+
+        if (c1 == '[') {
+            int c2 = readKey();
+            if (c2 == 'A') return key_code::up;
+            if (c2 == 'B') return key_code::down;
+            if (c2 == 'C') return key_code::right;
+            if (c2 == 'D') return key_code::left;
+        }
+        return key_code::none;
+    }
+
+    if (c == 'w'){
+		return key_code::w;
+	}
+    if (c == 'a'){
+		return key_code::a;
+	}
+    if (c == 's'){
+		return key_code::s;
+	}
+    if (c == 'd'){
+		return key_code::d;
+	}
+
+    return key_code::none;
+}
+
+
+
+class terminal_raw_mode {
+    termios old_{};
+    bool active_ = false;
+
+public:
+    terminal_raw_mode() {
+        if (tcgetattr(STDIN_FILENO, &old_) == -1) return;
+
+        termios raw = old_;
+        raw.c_lflag &= ~(ICANON | ECHO);
+        raw.c_cc[VMIN]  = 0;
+        raw.c_cc[VTIME] = 1;
+
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) return;
+        active_ = true;
+    }
+
+    ~terminal_raw_mode() {
+        if (active_) tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_);
+    }
+
+    terminal_raw_mode(const terminal_raw_mode&) = delete;
+    terminal_raw_mode& operator=(const terminal_raw_mode&) = delete;
+};
 
 class Entity {
 	protected:
@@ -28,6 +104,26 @@ class Entity {
 		void list_attributes(){
 			std::cout << "name: " << name << " body: " << body << std::endl;
 		}
+		int x() const {
+			return pos_x;
+		}
+		int y() const {
+			return pos_y;
+		}
+		char get_body() const {
+			return body;
+		}
+
+		void move_by(int dx, int dy, int max_w, int max_h) {
+			int nx = pos_x + dx;
+			int ny = pos_y + dy;
+			if (0 <= nx && nx < max_w){
+				pos_x = nx;
+			}
+			if (0 <= ny && ny < max_h){
+				pos_y = ny;
+			}
+		}
 };
 
 class Player : public Entity {
@@ -35,7 +131,7 @@ class Player : public Entity {
 		using Entity::Entity;
 };
 
-class Enemy : Entity {
+class Enemy : public Entity {
 	public:
 		using Entity::Entity;
 
@@ -58,7 +154,20 @@ void clear_screen(std::vector< std::vector< char> >& screen){
     }
 }
 
+static void render_screen(const std::vector<std::vector<char>>& screen) {
+    ansi_clear_and_home();
+    for (int y = 0; y < (int)screen.size(); y++) {
+        for (int x = 0; x < (int)screen[y].size(); x++) {
+            std::cout << screen[y][x];
+        }
+        std::cout << '\n';
+    }
+    std::cout << "wasd move | press escape to quit\n";
+    std::cout.flush();
+}
+
 int main(){
+	terminal_raw_mode raw_guard;
 	Player p1;
 	Player p2(1,2, "Carlos", 'C');
 	Player p3(2,3, "Albert", 'A');
@@ -68,9 +177,15 @@ int main(){
 	//lets make a 64 by 64 screen.
 	//we will never delete 'pixels' on the screen, just replace them with new 'pixels'
 	//so 2d array or vector is fine
-	std::vector< std::vector< char> > screen(64, std::vector<char>(64, '#'));
+	int h = 20;
+	int w = 40;
+	// std::vector< std::vector< char> > screen(64, std::vector<char>(64, '#'));
+	std::vector<std::vector<char>> screen(h, std::vector<char>(w, ' '));
 
-	std::vector< std::vector< Entity> > game_state(64, std::vector<char>(64));
+	std::vector< std::unique_ptr<Entity> > entities;
+	entities.push_back(std::make_unique<Player>(p1));
+	entities.push_back(std::make_unique<Player>(p2));
+	entities.push_back(std::make_unique<Enemy> (e2));
 	//lets add our obejcts into the game_state
 
 	//game loop
@@ -78,21 +193,40 @@ int main(){
 	while( !escape_pressed ) {
 		//clear the screen
 		clear_screen(screen);
+
 		//get the player input
-		int key = readKey();
-		if(key != -1) {
-			std::cout << "you pressed: "<< key << "\n";
+		key_code key = read_key_code();
+		if (key == key_code::esc) {
+			escape_pressed = true;
 		}
-		//
+
+		if (entities.size() >= 2) {
+			// p1  WASD
+			if (key == key_code::w) entities[0]->move_by(0, -1, w, h);
+			if (key == key_code::s) entities[0]->move_by(0,  1, w, h);
+			if (key == key_code::a) entities[0]->move_by(-1, 0, w, h);
+			if (key == key_code::d) entities[0]->move_by(1,  0, w, h);
+
+			// p2  arrow keys
+			if (key == key_code::up)    entities[1]->move_by(0, -1, w, h);
+			if (key == key_code::down)  entities[1]->move_by(0,  1, w, h);
+			if (key == key_code::left)  entities[1]->move_by(-1, 0, w, h);
+			if (key == key_code::right) entities[1]->move_by(1,  0, w, h);
+		}
 		//compute what is on the screen
-		for(Entity things : game_state) {
-			screen[things.pos_x][things.pos_y] = things.body;
-		}
+		for (const auto& e : entities) {
+            int x = e->x();
+            int y = e->y();
+            if (0 <= y && y < (int)screen.size() && 0 <= x && x < (int)screen[y].size()) {
+                screen[y][x] = e->get_body();
+            }
+        }
 
 		//render the game
-		print_screen(screen);
+		render_screen(screen);
 	}
-
+	ansi_clear_and_home();
+    std::cout << "bye\n";
 
 	return 0;
 }
